ec188b6efbd79e652c50b96c402e5ab237e31cd6
Arkadeepto-Das <arkadeepto.das@innoraft.com>
2023-03-07 10:20:11 +0530

    FT2023-55:Added indentation to PHP tag

--- task-1_oops/Check.php
+++ task-1_oops/Check.php
@@ -1,54 +1,54 @@
 <!-- OOPS and checking Part -->
 
 <?php
-class Check {
+  class Check {
 
-  public $firstName;
-  public $lastName;
-  public $c = 0;
+    public $firstName;
+    public $lastName;
+    public $c = 0;
 
-  function __construct($firstName, $lastName) {
-    $this->firstName = $firstName;
-    $this->lastName = $lastName;
-  }
-
-  function firstNamecheck() {
-    
-    if(!preg_match("/^[A-Z]{1}[a-z]+$/",$this->firstName)) {
-      
-      $fnameErr = "First name should start with capital letter and should contain only alphabets";
-      ++$this->c;
-      return $fnameErr;
-      
+    function __construct($firstName, $lastName) {
+      $this->firstName = $firstName;
+      $this->lastName = $lastName;
     }
-  
-  }
 
-  function lastNameCheck() {
-
-    if(!preg_match("/^[A-Z]{1}[a-z]+$/",$this->lastName)) {
+    function firstNamecheck() {
+      
+      if(!preg_match("/^[A-Z]{1}[a-z]+$/",$this->firstName)) {
         
-      $lnameErr = "Last name should start with capital letter and should contain only alphabets";
-      ++$this->c;
-      return $lnameErr;
-      
-    }
-
-  }
-
-  function fullName() {
+        $fnameErr = "First name should start with capital letter and should contain only alphabets";
+        ++$this->c;
+        return $fnameErr;
+        
+      }
     
-    if($this->c == 0) {
-      return $this->firstName . " " . $this->lastName;
+    }
+
+    function lastNameCheck() {
+
+      if(!preg_match("/^[A-Z]{1}[a-z]+$/",$this->lastName)) {
+          
+        $lnameErr = "Last name should start with capital letter and should contain only alphabets";
+        ++$this->c;
+        return $lnameErr;
+        
+      }
+
+    }
+
+    function fullName() {
+      
+      if($this->c == 0) {
+        return $this->firstName . " " . $this->lastName;
+      }
+
+    }
+
+    public static function input($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
     }
 
   }
-
-  public static function input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-  }
-
-}
